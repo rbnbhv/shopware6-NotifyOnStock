9d9b76b5a79ce78ee95fe69c6d428c28b45c2c1d
Ruben Allenstein <ru.allenstein@reply.de>
2022-09-29 16:42:41 +0200

    [UPDATE] output text changed and test running email

--- NotifyOnStock/src/Storefront/Controller/ProductAlertController.php
+++ NotifyOnStock/src/Storefront/Controller/ProductAlertController.php
@@ -1,38 +1,51 @@
 <?php declare(strict_types=1);
 
-namespace ReplyNotifyOnStock\Storefront\Controller;
+namespace NotifyOnStock\Storefront\Controller;
 
+use NotifyOnStock\Service\MailService\MailService;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use ReplyNotifyOnStock\Service\ReadingData;
-use Shopware\Core\Framework\Context;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use NotifyOnStock\Service\ReadData;
+use NotifyOnStock\Service\WriteData;
 
 /**
  * @Route(defaults={"_routeScope"={"storefront"}})
  */
 class ProductAlertController extends StorefrontController
 {
-    private ReadingData $readingData;
     private EntityRepository $productRepository;
+    private ReadData $readingData;
+    private WriteData $writeData;
+    private MailService $emailService;
 
-    public function __construct(EntityRepository $productRepository, ReadingData $readingData)
+    public function __construct(EntityRepository $productRepository, ReadData $readingData, WriteData $writeData, MailService $emailService)
     {
-        $this->readingData = $readingData;
         $this->productRepository = $productRepository;
+        $this->readingData = $readingData;
+        $this->writeData = $writeData;
+        $this->emailService = $emailService;
     }
 
     /**
-     * @Route("/{productName}/{productNumber}/notification-registered", name="frontend.productalert", methods={"Post"})
+     * @Route("/{productName}/{productNumber}/notification-registered", name="frontend.productalert", methods={"post"})
      */
-    public function showExample(Context $context, EntityRepository $productRepository): Response
+    public function submit(Request $request): Response
     {
-        dd($this->readingData->readData($context, $productRepository));
+        $session = $request->getSession();
+        $request->request->set('b', '1');
+//      For testing without scheduled Task:
+//        $this->emailService->process();
 
-        $this->renderStorefront('@ReplyNotifyOnStock/storefront/page/example.html.twig', [
-            'page' => $repositoryInfo
+        $textOutput = 'You have already been registered!';
+        if ($this->writeData->fillTableWithNewEntity($request, $session->get('context'))) {
+            $textOutput = 'You have been successfully registered to our notification alert. You will receive an Email soon!';
+        }
+
+        return $this->renderStorefront('@NotifyOnStock/storefront/page/example.html.twig', [
+            'text' => $textOutput
         ]);
     }
 }
