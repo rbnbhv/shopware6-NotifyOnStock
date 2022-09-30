41ccc20cda39e5be8a439952f5bc4a0cc1f6367f
Ruben Allenstein <ru.allenstein@reply.de>
2022-09-30 14:02:28 +0200

    todos revised

--- NotifyOnStock/src/NotifyOnStock.php
+++ NotifyOnStock/src/NotifyOnStock.php
@@ -33,10 +33,8 @@ class NotifyOnStock extends Plugin
          *    - [] Wie speichern wir Daten in die Datenbanktabelle (Wie funktioniert die Migration?)
          *    - [] Einträge in Liste speichern (Form übergeben und Daten abspeichern)
          *    - [] Entitys festlegen für die Liste
-         *
          *    - [X] Datenbanktabelle erstellen für Notification
          *    - [] Datensätze in die Tabelle speichern / übergeben
-         *    - []
          */
     }
 }
